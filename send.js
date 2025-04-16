c0f195d48930cef999429d62d123b186e704111a
Jamie Davidson <jamie@davidson.sh>
2025-04-16 14:26:00 +0100

    update

--- send.js
+++ send.js
@@ -1,12 +1,12 @@
 import { connect } from 'amqplib';
 
 setInterval(async function sendMessage() {
-    connection = await connect('amqp://root:password@10.1.0.100');
+    let connection = await connect('amqp://root:password@10.1.0.100');
     channel = await connection.createChannel();
     queue = 'messages'
     message = 'Hello!'
 
     await channel.assertQueue(queue, { durable: false })
-    //console.log(`[x] Sent ${message}`)
+    console.log(`[x] Sent ${message}`)
     channel.sendToQueue(queue, Buffer.from(message + ' at ' + (new Date())))
 }, 50)
