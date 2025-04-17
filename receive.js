f621fc6d2a8fdf3187a3b18a7a4471aad6200e84
Jamie Davidson <jamie@davidson.sh>
2025-04-17 16:18:32 +0100

    save

--- receive.js
+++ receive.js
@@ -1,4 +1,12 @@
 import { connect } from 'amqplib';
+import { createClient } from 'redis';
+const client = createClient({
+    url: "redis://redis:6379"
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+await client.connect();
 
 const connection = await connect('amqp://root:password@10.1.0.100');
 
@@ -10,5 +18,6 @@ await channel.assertQueue(queue, { durable: false })
 
 channel.consume(queue, (msg) => {
     //console.log(`[x] Received ${msg.content.toString()}`)
+    client.del(msg.content.toString());
     channel.ack(msg)
 })
--- send.js
+++ send.js
@@ -1,12 +1,23 @@
 import { connect } from 'amqplib';
+import * as crypto from "node:crypto";
+import { createClient } from 'redis';
+
+const client = createClient({
+    url: "redis://redis:6379"
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+await client.connect();
 
 setInterval(async function sendMessage() {
     let connection = await connect('amqp://root:password@10.1.0.100', { timeout: 5000 });
     let channel = await connection.createChannel();
     let queue = 'messages'
-    let message = 'Hello!  at ' + (new Date())
-
+    let randstring = crypto.randomBytes(20).toString('hex');
+    let message = randstring
+    await client.set(randstring, 'value');
     await channel.assertQueue(queue, { durable: false })
-    console.log(`[x] Sent ${message}`)
+    console.log(`[x] Inserted ${randstring} at ` + (new Date()))
     channel.sendToQueue(queue, Buffer.from(message))
 }, 50)
